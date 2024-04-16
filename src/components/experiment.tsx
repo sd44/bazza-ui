c31d82f5279d56db685d4964227cf4b54751053c
Kian Bazarjani <kian@bazza.dev>
2024-04-16 18:49:43 -0400

    Show status of experiment

--- src/components/experiment.tsx
+++ src/components/experiment.tsx
@@ -1,6 +1,5 @@
 import { TExperiment } from '@/data/experiments'
 import { Badge } from './ui/badge'
-import Prototype from './prototype'
 import PrototypeGallery from './prototype-gallery'
 
 export default function Experiment({
@@ -8,10 +7,15 @@ export default function Experiment({
 }: {
   experiment: TExperiment
 }) {
-  const { title, description, tags, prototypes } = experiment
+  const { title, description, status, tags, prototypes } = experiment
   return (
     <div className="flex flex-col lg:flex-row lg:justify-between gap-6 lg:gap-12 xl:gap-24">
       <div className="lg:max-w-[250px] space-y-4">
+        {status === 'Final' ? (
+          <Badge variant="default">Final Design</Badge>
+        ) : (
+          <Badge variant="outline">In Progress</Badge>
+        )}
         <h2 className="font-semibold text-lg font-mono tracking-tighter">
           {title}
         </h2>
--- src/data/experiments.tsx
+++ src/data/experiments.tsx
@@ -1,10 +1,10 @@
-import Page from '@/app/page'
 import * as WorkSessionCard from '@/components/experiments/work-session-card'
 
 export type TExperiment = {
   title: string
   description: string
   tags: string[]
+  status: 'WIP' | 'Final'
   prototypes: TPrototype[]
 }
 
@@ -18,6 +18,7 @@ export const experimentsData: TExperiment[] = [
     title: 'Work session card',
     description: `Shows some initial information regarding a user's work session, such as time range, related project, and more. The card should expand to reveal additional information, such as the notes taken during the work session.`,
     tags: ['react', 'tailwindcss', 'next.js', 'framer motion'],
+    status: 'WIP',
     prototypes: [
       {
         component: <WorkSessionCard.V1 />,
