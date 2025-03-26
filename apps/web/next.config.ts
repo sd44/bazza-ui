ca9986d33cd7b66ea7367fb7677470c3a059d827
Kian Bazza <kian@bazza.dev>
2025-03-26 17:16:14 -0400

    chore: setup basic redirects (#10)

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,6 +2,28 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   transpilePackages: ['next-mdx-remote'],
+  redirects: async () => [
+    {
+      source: '/docs',
+      destination: '/docs/intro',
+      permanent: false,
+    },
+    {
+      source: '/filters',
+      destination: '/docs/data-table-filter',
+      permanent: false,
+    },
+    {
+      source: '/r/filters',
+      destination: '/r/data-table-filter.json',
+      permanent: false,
+    },
+    {
+      source: '/r/data-table-filter',
+      destination: '/r/data-table-filter.json',
+      permanent: false,
+    },
+  ],
 }
 
 export default nextConfig
