055a50ee56444d9c19d4b558d27ac600f970a7d8
Kian Bazza <kian@bazza.dev>
2025-04-12 23:34:46 -0400

    chore(fonts): use real italics for Inter (#60)

--- apps/web/lib/fonts.ts
+++ apps/web/lib/fonts.ts
@@ -1,23 +1,16 @@
-import { Geist_Mono, Inter } from 'next/font/google'
+import { Inter } from 'next/font/google'
 import localFont from 'next/font/local'
 
-const inter = Inter({
+export const inter = Inter({
+  style: ['normal', 'italic'],
   axes: ['opsz'],
   subsets: ['latin'],
   variable: '--font-inter',
   display: 'swap',
 })
 
-const geistMono = Geist_Mono({
-  subsets: ['latin'],
-  variable: '--font-geist-mono',
-  display: 'swap',
-})
-
-const berkeleyMono = localFont({
+export const berkeleyMono = localFont({
   src: '../assets/fonts/berkeley-mono/BerkeleyMono-Variable.woff2',
   variable: '--font-berkeley-mono',
   display: 'swap',
 })
-
-export { inter, geistMono, berkeleyMono }
