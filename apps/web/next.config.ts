a065711989ef6b78894781bdd26e229bfe54d891
Kian Bazza <kian@bazza.dev>
2025-07-30 15:07:28 -0400

    build: use separate `.next` dirs for production vs. dev (#139)

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  distDir: process.env.NODE_ENV === 'production' ? '.next' : '.next-dev',
   transpilePackages: ['next-mdx-remote'],
   redirects: async () => [
     {
