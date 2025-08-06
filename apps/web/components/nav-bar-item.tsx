5dd73f93c72090bdeedc00f6362a3b9449f70f26
Kian Bazza <kian@bazza.dev>
2025-08-05 22:44:23 -0400

    chore: cleanup console logs from <NavBarItem /> (#162)

--- apps/web/components/nav-bar-item.tsx
+++ apps/web/components/nav-bar-item.tsx
@@ -1,9 +1,9 @@
 'use client'
 
-import { Button } from '@/components/ui/button'
-import { cn } from '@/lib/utils'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import { Button } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
 
 type NavBarItemProps = {
   href: string
@@ -13,9 +13,6 @@ type NavBarItemProps = {
 export function NavBarItem({ href, label, ...props }: NavBarItemProps) {
   const pathname = usePathname()
   const isActive = pathname === href || pathname.startsWith(href)
-
-  console.log('href:', href)
-  console.log('pathname:', pathname)
 
   return (
     <Button variant="ghost" size="sm" asChild>
